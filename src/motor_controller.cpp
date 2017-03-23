d7d92f7b5d0a6cd13fe850724102bdcf84dc75c6
Bingjie <jienet888@gmail.com>
2017-03-23 17:10:22 +0000

    controllers work, instatiations dont

--- src/motor_controller.cpp
+++ src/motor_controller.cpp
@@ -6,142 +6,131 @@
 #include  "parser.h"
 
 namespace controller {
-	// Serial output for debugging
-	static Serial debug(SERIAL_TX, SERIAL_RX, 9600);
+    // Serial output for debugging
+    static Serial debug(SERIAL_TX, SERIAL_RX, 9600);
 
-	// Variable output for motor driver
-	float duty_cycle = 50;
+    // Variable output for motor driver
+    float duty_cycle = 50;
 
 
-	// Position and velocity control variables
-	static float error = 0;
-	static float error_int = 0;
+    // Position and velocity control variables
+    static float error_p = 0;
+    static float error_v = 0;
+    static float error_int = 0;
 
-	static float pos_k = 100;
-	static float pos_kp = 1;
-	static float pos_kv = -0.75;
+    static const float pos_k = 100;
+    static const float pos_kp = 1;
+    static const float pos_kv = -0.75;
 
-	static float vel_k = 10;
-	static float vel_kp = 1;
-	static float vel_ki = 0.005;
+    static const float vel_k = 1.25;
+    static const float vel_kp = 1.5;
+    static const float vel_ki = 0.45;
 
-	static parser::update_t curr_op = parser::OP_NIL;
+    static parser::update_t curr_op = parser::OP_NIL;
 
-	// Control functions which calculate the next duty cycle value
-	static float vController(const float target_v, const float actual_v, float dt){
-		float prev_error = error;
-		error = target_v - actual_v;
-		error_int += (error + prev_error) / 2 * dt;
+    // Control functions which calculate the next duty cycle value
+    static float vController(const float target_v, const float actual_v, const float dt){
+        float prev_error = error_v;
+        error_v = target_v - actual_v;
+        error_int += (error_v + prev_error) / 2 * dt;
 
-		float n = vel_kp * error + vel_ki * error_int; // pos_kd * error_delta; + pos_ki * error_int;
-		float res = vel_k * n;
-		res = (res > 100) ? 100 : res;
-		res = (res < -100) ? -100 : res;
-		return res;
-	}
+        float n = vel_kp * error_v + vel_ki * error_int; // pos_kd * error_delta; + pos_ki * error_int;
+        float res = vel_k * n;
+        res = (res > 100) ? 100 : res;
+        res = (res < -100) ? -100 : res;
+        return res;
+    }
 
-	static float pController(const float target_p, const float actual_p, const float actual_v){
+    static float pController(const float target_p, const float actual_p, const float actual_v){
 
-		error = target_p - actual_p;
+        error_p = target_p - actual_p;
 
-		float n = pos_kp * error + pos_kv * actual_v; 
-		float res = pos_k * n;
+        float n = pos_kp * error_p + pos_kv * actual_v; 
+        float res = pos_k * n;
 
-		res = (res > 100) ? 100 : res;
-		res = (res < -100) ? -100 : res;
+        res = (res > 100) ? 100 : res;
+        res = (res < -100) ? -100 : res;
 
-		return res;
-	}
+        return res;
+    }
 
-	static float pvController(const float target_p, const float actual_p, const float target_v, const float actual_v, const float dt){
-        
-        float v_duty = vController(actual_v, target_v, dt);
+    static float pvController(const float target_p, const float actual_p, const float target_v, const float actual_v, const float dt){
+        float t_v = (target_p > 0) ? target_v : -target_v;
+        float v_duty = vController(t_v,  actual_v, dt);
         float p_duty = pController(target_p, actual_p, actual_v);
 
+//        v_duty = (target_p > 0) ? std::abs(v_duty) : -std::abs(v_duty);
         // yes, the math works out
-        v_duty = (p_duty > 0) ? std::abs(v_duty) : -std::abs(v_duty);
-        return (target_v - std::abs(actual_v) > 0) ? p_duty : v_duty;
+        
+        return (std::abs(v_duty) < std::abs(p_duty)) ? v_duty : p_duty;
+        //return (std::abs(v_duty) < std::abs(p_duty)) ? v_duty : p_duty;//(target_v - std::abs(actual_v) > 0) ? p_duty : v_duty;
 
-	static void reset(){
+//      return (target_v - std::abs(actual_v) > 0) ? p_duty : v_duty;
+    }
 
-		error_int = 0;
-	}
+    static void reset(){
 
-	void nextStep(){
-		Timer control_loop;
-		Timer pos_loop;
+        error_int = 0;
+    }
 
-		float actual_p;
-		float actual_v;
-		float target_p;
-		float target_v;
-		float dt;
+    void nextStep(){
+        Timer control_loop;
+        Timer pos_loop;
 
-		control_loop.start();
-		pos_loop.start();
+        // NEED TO SET INITIAL VALUES
+        float actual_p = 0;
+        float actual_v = 0;
+        float target_p = 0;
+        float target_v = 0;
+        float dt = 1;
 
-		while (1) {
-			if (parser::ready[CTRL_INDEX]) {
-				parser::update_t new_op = parser::op_code;
-				if ((new_op == parser::OP_POS) || (new_op == parser::OP_VEL) || (new_op != parser::OP_PV)) {
-					curr_op = new_op;
-					
-					reset();
-					
-					new_op = parser::OP_NIL;
-					target_p = parser::target_pos;
-					target_v = parser::target_vel;
-				} 
-				parser::ready[CTRL_INDEX] = false;
-			}
+        duty_cycle = 50;
+        error_p = 0;
+        error_v = 0;
+        curr_op = parser::OP_NIL;
+        control_loop.start();
+        pos_loop.start();
+
+        while (1) {
+            if (parser::ready[CTRL_INDEX]) {
+                parser::update_t new_op = parser::op_code;
+                if ((new_op == parser::OP_POS) || (new_op == parser::OP_VEL) || (new_op == parser::OP_PV)) {
+                    curr_op = new_op;
+                    
+                    reset();
+                    
+                    new_op = parser::OP_NIL;
+                    target_p = parser::target_pos;
+                    target_v = parser::target_vel;
+                } 
+                parser::ready[CTRL_INDEX] = false;
+            }
 
 
-			switch (curr_op) {
-				case parser::OP_VEL:
-					// Get input from other parts
-					actual_v = odometer::velocity;
+            actual_p = odometer::position;
+            actual_v = odometer::velocity;
 
-					dt = pos_loop.read();
-					pos_loop.reset();
-				
-					// Calculate next step
-					float res = vController(target_v, actual_v, dt);
+            dt = pos_loop.read();
+            pos_loop.reset();
+            float res = 0;
 
-					duty_cycle = res;
-					break;
+            if(curr_op == parser::OP_VEL) {
+                res = vController(target_v, actual_v, dt);
+            
+            } else if(curr_op == parser::OP_POS) {
+                res = pController(target_p, actual_p, actual_v);
+            
+            } else if(curr_op == parser::OP_PV) {
+                res = pvController(target_p, actual_p, target_v, actual_v, dt);
+            
+            }
 
-				case parser::OP_POS:
-					actual_p = odometer::position;
-					actual_v = odometer::velocity;				  
+            duty_cycle = res;
 
-					// Calculate next step
-					res = pController(target_p, actual_p, actual_v);
-
-					duty_cycle = res;
-					break;
-
-				case parser::OP_PV:
-					actual_v = odometer::velocity;				  
-					actual_p = odometer::position;
-
-					dt = pos_loop.read();
-					pos_loop.reset();
-
-					// Calculate next step
-					res = pvController(target_p, actual_p, target_v, actual_v, dt);
-
-					duty_cycle = res;
-					break;
-					
-				default:
-					// Do nothing otherwise
-					break;
-			}
-
-			int readout = control_loop.read() * 1000;
-			control_loop.reset();
-			Thread::wait((readout < 20) ? (20 - readout) : 0);
-				
-		}
-	}
+            int readout = control_loop.read() * 1000;
+            control_loop.reset();
+            Thread::wait((readout < 20) ? (20 - readout) : 0);
+                
+        }
+    }
 }
--- src/parser.cpp
+++ src/parser.cpp
@@ -4,257 +4,257 @@
 
 // I know I'm treating namespaces as classes, I beg for your mercy if this is sacrilegious
 namespace parser {
-	// Serial object
-	RawSerial pc(SERIAL_TX, SERIAL_RX);
+    // Serial object
+    RawSerial pc(SERIAL_TX, SERIAL_RX);
 
-	// Serial Input variables
-	static char input_buffer[BUFF_SIZE];
-	static volatile int input_counter = 0;
-	static volatile bool input_ready = false;
+    // Serial Input variables
+    static char input_buffer[BUFF_SIZE];
+    static volatile int input_counter = 0;
+    static volatile bool input_ready = false;
 
-	// Parsed Output to be used by other parts of the code
-	float target_pos = 100;
-	float target_vel = 10;
-	float tune_period = 0.5;
-	update_t op_code = OP_POS;
-	int tunes_list[TUNE_BUFFER];
-	volatile bool ready[3] = {false, false, false};
+    // Parsed Output to be used by other parts of the code
+    float target_pos = -256;
+    float target_vel = 10;
+    float tune_period = 0.5;
+    update_t op_code = OP_VEL;
+    int tunes_list[TUNE_BUFFER];
+    volatile bool ready[3] =  {true, true, true};//{false, false, false};
 
-	// For Tune setting
-	static const int *note_half_period_map[8];
+    // For Tune setting
+    static const int *note_half_period_map[8];
 
-	// Mapping of half periods of each note
-	static const int error[] = {0,0,0};
-	static const int A[] = {1204, 1136, 1073}; 	//A^ , A, A#
-	static const int B[] = {1073, 1012, 0}; 	//B^ , B, B#
-	static const int C[] = {0, 1911, 1804}; 	//C^ , C, C#
-	static const int D[] = {1804, 1703, 1607}; 	//D^ , D, D#
-	static const int E[] = {1607, 1517, 0}; 	//E^ , E, E#
-	static const int F[] = {0, 1432, 1351}; 	//F^ , F, F#
-	static const int G[] = {1351, 1273, 1204};  //G^ , G, G#
+    // Mapping of half periods of each note
+    static const int error[] = {0,0,0};
+    static const int A[] = {1204, 1136, 1073};  //A^ , A, A#
+    static const int B[] = {1073, 1012, 0};  //B^ , B, B#
+    static const int C[] = {0, 1911, 1804};  //C^ , C, C#
+    static const int D[] = {1804, 1703, 1607};  //D^ , D, D#
+    static const int E[] = {1607, 1517, 0};  //E^ , E, E#
+    static const int F[] = {0, 1432, 1351};  //F^ , F, F#
+    static const int G[] = {1351, 1273, 1204};  //G^ , G, G#
 
 
-	// Parser Functions
-	static void serial_isr(){
-		if (pc.readable() && (input_counter < BUFF_SIZE) && (!input_ready)) {
-			input_buffer[input_counter++] = pc.getc();
-			pc.putc(input_buffer[input_counter-1]); 
-			
-			if (input_buffer[input_counter-1] == '\r') {			
-				pc.putc('\n'); 
-				input_ready = true;
-			}   
-		} 
-	}
+    // Parser Functions
+    static void serial_isr(){
+        if (pc.readable() && (input_counter < BUFF_SIZE) && (!input_ready)) {
+            input_buffer[input_counter++] = pc.getc();
+            pc.putc(input_buffer[input_counter-1]); 
+            
+            if (input_buffer[input_counter-1] == '\r') {            
+                pc.putc('\n'); 
+                input_ready = true;
+            }   
+        } 
+    }
 
-	static int parseCommand(const int _length){
-		// Parsing variables
-		char op_char = 0;
-		char float_buffer[8];
-		int note_buffer[3];
+    static int parseCommand(const int _length){
+        // Parsing variables
+        char op_char = 0;
+        char float_buffer[8];
+        int note_buffer[3];
 
-		// array indices
-		int count = 0;
-		int note_count = 0;
-		int float_buffer_ind = 0;
+        // array indices
+        int count = 0;
+        int note_count = 0;
+        int float_buffer_ind = 0;
 
-		// Update variables, so that previous values will not be overwritten if function encounters an invalid input
-		uint8_t new_op = 0;
-		float new_pos = 0;
-		float new_vel = 0;
-		float bpm = 60;
-		int new_tunes[TUNE_BUFFER];
+        // Update variables, so that previous values will not be overwritten if function encounters an invalid input
+        uint8_t new_op = 0;
+        float new_pos = 0;
+        float new_vel = 0;
+        float bpm = 60;
+        int new_tunes[TUNE_BUFFER];
 
-		// Checks if string input is empty (except for '\r')
-		if (_length <= 1){
-			return 0;
-		}
-		
-		// Initialising buffers
-		std::memset(float_buffer, 0, 8);
-		std::memset(note_buffer, 0, 3);
-		std::memset(tunes_list, 0, TUNE_BUFFER * sizeof(int));
-		note_buffer[1] = 1;
+        // Checks if string input is empty (except for '\r')
+        if (_length <= 1){
+            return 0;
+        }
+        
+        // Initialising buffers
+        std::memset(float_buffer, 0, 8);
+        std::memset(note_buffer, 0, 3);
+        std::memset(tunes_list, 0, TUNE_BUFFER * sizeof(int));
+        note_buffer[1] = 1;
 
-		// Iterating through input_buffer
-		while (count < _length) {
-			char current_char = input_buffer[count++];
-			
-			switch(current_char){
-				case 'R':
-					if ((op_char == 'V') || (op_char == 'R') || (op_char == 'T') || (op_char == 'M')) {
-						return 0;
-					} else {
-						op_char = current_char; 
-					}
-					break;
+        // Iterating through input_buffer
+        while (count < _length) {
+            char current_char = input_buffer[count++];
+            
+            switch(current_char){
+                case 'R':
+                    if ((op_char == 'V') || (op_char == 'R') || (op_char == 'T') || (op_char == 'M')) {
+                        return 0;
+                    } else {
+                        op_char = current_char; 
+                    }
+                    break;
 
-				case 'V':
-					if (op_char == 'R') {
-						op_char = 'V';
-						new_pos = (float)std::atof(float_buffer);
-						std::memset(float_buffer,0,8);
-						float_buffer_ind = 0;
-						new_op |= 0x01;
+                case 'V':
+                    if (op_char == 'R') {
+                        op_char = 'V';
+                        new_pos = (float)std::atof(float_buffer);
+                        std::memset(float_buffer,0,8);
+                        float_buffer_ind = 0;
+                        new_op |= 0x01;
 
-					} else if ((op_char == 'V') || (op_char == 'T') || (op_char == 'M')){
-						return 0;
-					} else {
-						op_char = current_char; 
-					}
-					break;
-				
-				case 'T':
-					if ((op_char == 'V') || (op_char == 'R') || (op_char == 'M')) {
-						return 0;
-					} else {
-						op_char = current_char; 
-					}
-					break;
+                    } else if ((op_char == 'V') || (op_char == 'T') || (op_char == 'M')){
+                        return 0;
+                    } else {
+                        op_char = current_char; 
+                    }
+                    break;
+                
+                case 'T':
+                    if ((op_char == 'V') || (op_char == 'R') || (op_char == 'M')) {
+                        return 0;
+                    } else {
+                        op_char = current_char; 
+                    }
+                    break;
 
-				case 'M':
-					if ((op_char == 'V') || (op_char == 'R') || (op_char == 'T') ) {
-						return 0;
-					} else {
-						op_char = current_char;
-					}
-					break;
+                case 'M':
+                    if ((op_char == 'V') || (op_char == 'R') || (op_char == 'T') ) {
+                        return 0;
+                    } else {
+                        op_char = current_char;
+                    }
+                    break;
 
-				case '\r':
-					if(op_char == 'R'){
-						new_pos = (float)std::atof(float_buffer);
-						new_op |= 0x01;
+                case '\r':
+                    if(op_char == 'R'){
+                        new_pos = (float)std::atof(float_buffer);
+                        new_op |= 0x01;
 
-					} else if (op_char == 'V') {
-						new_vel = (float)std::atof(float_buffer);
-						new_op |= 0x02;
+                    } else if (op_char == 'V') {
+                        new_vel = (float)std::atof(float_buffer);
+                        new_op |= 0x02;
 
-					} else if (op_char == 'T') {
-						new_op |= 0x04;
+                    } else if (op_char == 'T') {
+                        new_op |= 0x04;
 
-					} else if (op_char == 'M') {
-						new_tune_period = (float)std::atof(float_buffer);
-						new_op = 5;
-					}
-					
-					break;
+                    } else if (op_char == 'M') {
+                        bpm = (float)std::atof(float_buffer);
+                        new_op = 5;
+                    }
+                    
+                    break;
 
-				default:
-					if (op_char == 'T') {
-						if ((current_char == 'A') || (current_char == 'B') || 
-							(current_char == 'C') || (current_char == 'D') || 
-							(current_char == 'E') || (current_char == 'F') || 
-							(current_char == 'G')) {
-							note_buffer[0] = int(current_char - 'A') + 1;
-						}				
-						if (current_char == '^'){
-							note_buffer[1] = 0;
-						} else if (current_char == '#') {
-							note_buffer[1] = 2;
-						}
+                default:
+                    if (op_char == 'T') {
+                        if ((current_char == 'A') || (current_char == 'B') || 
+                            (current_char == 'C') || (current_char == 'D') || 
+                            (current_char == 'E') || (current_char == 'F') || 
+                            (current_char == 'G')) {
+                            note_buffer[0] = int(current_char - 'A') + 1;
+                        }              
+                        if (current_char == '^'){
+                            note_buffer[1] = 0;
+                        } else if (current_char == '#') {
+                            note_buffer[1] = 2;
+                        }
 
-						if ((current_char == '1') || (current_char == '2') || 
-							(current_char == '3') || (current_char == '4') || 
-							(current_char == '5') || (current_char == '6') || 
-							(current_char == '7') || ( current_char == '8')) {
-							int reps = int(current_char - '0');
+                        if ((current_char == '1') || (current_char == '2') || 
+                            (current_char == '3') || (current_char == '4') || 
+                            (current_char == '5') || (current_char == '6') || 
+                            (current_char == '7') || ( current_char == '8')) {
+                            int reps = int(current_char - '0');
 
-							int parsed_note = note_half_period_map[note_buffer[0]][note_buffer[1]];
-							
-							if (parsed_note == 0) {
-								// pc.printf("Invalid note\n\r");
-								return 0;
-							} 
-							
-							if (note_buffer[1] != 1){
-								// pc.printf("parsed note: %c%c, duration: %d, half_period: %d\n\r", 'A' + note_buffer[0] - 1, temp_char, reps, parsed_note);
-								char temp_char = (note_buffer[1] == 0) ? '^' : '#';
-							} else {
-								// pc.printf("parsed note: %c, duration: %d, half_period: %d\n\r", 'A' + note_buffer[0] - 1, reps, parsed_note);				
-							}
+                            int parsed_note = note_half_period_map[note_buffer[0]][note_buffer[1]];
+                            
+                            if (parsed_note == 0) {
+                                // pc.printf("Invalid note\n\r");
+                                return 0;
+                            } 
+                            
+                            // if (note_buffer[1] != 1){
+                            //  pc.printf("parsed note: %c%c, duration: %d, half_period: %d\n\r", 'A' + note_buffer[0] - 1, temp_char, reps, parsed_note);
+                            //  char temp_char = (note_buffer[1] == 0) ? '^' : '#';
+                            // } else {
+                            //  pc.printf("parsed note: %c, duration: %d, half_period: %d\n\r", 'A' + note_buffer[0] - 1, reps, parsed_note);               
+                            // }
 
-							for (int i = 0; i < reps; i++){
-								new_tunes[note_count++] = parsed_note;
-							}
+                            for (int i = 0; i < reps; i++){
+                                new_tunes[note_count++] = parsed_note;
+                            }
 
-							// Reset buffers
-							std::memset(note_buffer, 0, 3);
-							note_buffer[1] = 1;
-						}
-					} else {
-						float_buffer[float_buffer_ind++] = current_char;
-					}
-					break;
-			}
-		}
+                            // Reset buffers
+                            std::memset(note_buffer, 0, 3);
+                            note_buffer[1] = 1;
+                        }
+                    } else {
+                        float_buffer[float_buffer_ind++] = current_char;
+                    }
+                    break;
+            }
+        }
 
-		switch (new_op){
-			case 1:
-				op_code = OP_POS;
-				break;
-			case 2:
-				op_code = OP_VEL;
-				break;
-			case 3:
-				op_code = OP_PV;
-				break;
-			case 4:
-				op_code = OP_TUNE;
-				break;
-			default:
-				op_code = OP_NIL;
-				break;
-		}
+        switch (new_op){
+            case 1:
+                op_code = OP_POS;
+                break;
+            case 2:
+                op_code = OP_VEL;
+                break;
+            case 3:
+                op_code = OP_PV;
+                break;
+            case 4:
+                op_code = OP_TUNE;
+                break;
+            default:
+                op_code = OP_NIL;
+                break;
+        }
 
-		// Update state
+        // Update state
 
-		if (op_code != OP_NIL){
-			target_pos = new_pos;
-			target_vel = new_vel;
-			tune_period = 60 / bpm;
-			for (int i = 0; i < 16; i++) {
-				tunes_list[i] = new_tunes[i];
-			}
-		}
+        if (op_code != OP_NIL){
+            target_pos = new_pos;
+            target_vel = new_vel;
+            tune_period = 60 / bpm;
+            for (int i = 0; i < 16; i++) {
+                tunes_list[i] = new_tunes[i];
+            }
+        }
 
-		return 1;
-	}
+        return 1;
+    }
 
-	void pollSerialIn(){
-		while(1){
-			if (input_ready) {
-				int length = input_counter;
-				
-				// pc.printf("strlen: %d, echo: %s\n", length, input_buffer); 
-				
-				if(parseCommand(length)){
-					// Unrolling for faster assignment
-					ready[0] = true;
-					ready[1] = true;
-					ready[2] = true;
+    void pollSerialIn(){
+        while(1){
+            if (input_ready) {
+                int length = input_counter;
+                
+                // pc.printf("strlen: %d, echo: %s\n", length, input_buffer); 
+                
+                if(parseCommand(length)){
+                    // Unrolling for faster assignment
+                    ready[0] = true;
+                    ready[1] = true;
+                    ready[2] = true;
 
-				}
+                }
 
-				std::memset(input_buffer, 0 , BUFF_SIZE);
-				input_ready = false;
-				input_counter = 0;
-			}
+                std::memset(input_buffer, 0 , BUFF_SIZE);
+                input_ready = false;
+                input_counter = 0;
+            }
 
-			Thread::wait(500);
-		}
-	}
+            Thread::wait(500);
+        }
+    }
 
-	void init(){
-		pc.baud(9600);
-		pc.attach(&serial_isr);
-			
-		note_half_period_map[0] = error; 
-		note_half_period_map[1] = A; 
-		note_half_period_map[2] = B; 
-		note_half_period_map[3] = C;
-		note_half_period_map[4] = D;
-		note_half_period_map[5] = E;
-		note_half_period_map[6] = F;
-		note_half_period_map[7] = G;  
-	}
+    void init(){
+        pc.baud(9600);
+        pc.attach(&serial_isr);
+            
+        note_half_period_map[0] = error; 
+        note_half_period_map[1] = A; 
+        note_half_period_map[2] = B; 
+        note_half_period_map[3] = C;
+        note_half_period_map[4] = D;
+        note_half_period_map[5] = E;
+        note_half_period_map[6] = F;
+        note_half_period_map[7] = G;  
+    }
 }
