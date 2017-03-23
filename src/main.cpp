523abd2ad1c068f7aa610b4c8ffa5d7e7bd59354
Bingjie <jienet888@gmail.com>
2017-03-23 18:33:41 +0000

    fixed serial bug

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,14 @@
 #include "odometer.h"
 #include "parser.h"
 
-// Thread instantiations
-Thread parser_thread(osPriorityNormal, 640);
+Thread parser_thread(osPriorityNormal, 1024);
 Thread odometer_thread(osPriorityNormal, 300);
 Thread controller_thread(osPriorityNormal, 600);
 Thread driver_thread(osPriorityNormal, 400);
 
 //Main
 int main() {
-    Serial pc(SERIAL_TX, SERIAL_RX);
-    pc.baud(9600);
+    RawSerial pc = parser::pc;
 
     parser::init();
     odometer::init();
@@ -22,28 +20,54 @@ int main() {
 
     parser_thread.start(parser::pollSerialIn);
     odometer_thread.start(odometer::updateState);
-    controller_thread.start(controller::nextStep);    
+    controller_thread.start(controller::nextStep);  
     driver_thread.start(driver::runMotor);
     
+    // Variables from parser 
+    parser::update_t read_op;
+    float read_tp;
+    float read_tv;
+    float read_tunep;
+    int read_tunes[TUNE_BUFFER];
+
 
     
     while(1){
-       //  pc.printf("(Parser)     Total: %d, Free: %d, Used: %d(%d)\n\r", 
-       //      parser_thread.stack_size(), parser_thread.free_stack(), 
-       //      parser_thread.used_stack(), parser_thread.max_stack());
-       //  pc.printf("(Odometer)   Total: %d, Free: %d, Used: %d(%d)\n\r", 
-       //      odometer_thread.stack_size(), odometer_thread.free_stack(), 
-       //      odometer_thread.used_stack(), odometer_thread.max_stack());
-       //  pc.printf("(Controller) Total: %d, Free: %d, Used: %d(%d)\n\r", 
-       //     controller_thread.stack_size(), controller_thread.free_stack(), 
-       //     controller_thread.used_stack(), controller_thread.max_stack());
-       //  pc.printf("(Driver)     Total: %d, Free: %d, Used: %d(%d)\n\r", 
-       // driver_thread.stack_size(), driver_thread.free_stack(), 
-       // driver_thread.used_stack(), driver_thread.max_stack());
+        if (parser::ready[MAIN_INDEX]){
+            read_op = parser::op_code;
+            read_tp = parser::target_pos;
+            read_tv = parser::target_vel;
+            read_tunep = parser::tune_period;
+
+            for (int i = 0; i < TUNE_BUFFER; i++) {
+                read_tunes[i] = parser::tunes_list[i];
+            }
+
+            parser::ready[MAIN_INDEX] = false;
+            
+            if (read_op == parser::OP_NIL) {
+                pc.printf("Invalid Input\n\r");
+
+            } else if (read_op == parser::OP_POS) {
+                pc.printf("Target position set to %3.2f\n\r", read_tp);
+
+            } else if (read_op == parser::OP_VEL) {
+                pc.printf("Target velocity set to %3.2f\n\r", read_tv);
+
+            } else if (read_op == parser::OP_PV) {  
+                pc.printf("Target position set to %3.2f\n\r", read_tp);
+                pc.printf("Target velocity set to %3.2f\n\r", read_tv);
+
+            } else if (read_op == parser::OP_TUNE) {
+                pc.printf("Playing a tune~\n\r");
+
+            } else if (read_op == parser::OP_BPM) {
+                pc.printf("Set BPM to %3.2f\n\r", 60/read_tunep);
+
+            } 
+            
+        }            
         Thread::wait(1000);
-        pc.printf("op: %d, target_p: %3.2f, actual_p: %3.2f, target_v: %3.2f, actual_v: %3.2f, duty: %3.2f \n\r", 
-            parser::op_code, parser::target_pos, odometer::position, 
-            parser::target_vel, odometer::velocity, controller::duty_cycle);
     }
 
     return 0;
--- src/motor_driver.cpp
+++ src/motor_driver.cpp
@@ -9,180 +9,183 @@
 
 namespace driver {
 
-	//Drive state to output table
-	static const int8_t driveTable[] = {0x12,0x18,0x09,0x21,0x24,0x06,0x00,0x00};
-	static const int8_t stateMap[] = {0x07,0x05,0x03,0x04,0x01,0x00,0x02,0x07};  
+    //Drive state to output table
+    static const int8_t driveTable[] = {0x12,0x18,0x09,0x21,0x24,0x06,0x00,0x00};
+    static const int8_t stateMap[] = {0x07,0x05,0x03,0x04,0x01,0x00,0x02,0x07};  
 
-	//Digital Motor Drive outputs
-	static DigitalOut L1L(L1Lpin); 
-	static DigitalOut L1H(L1Hpin);
-	static DigitalOut L2L(L2Lpin);
-	static DigitalOut L2H(L2Hpin);
-	static DigitalOut L3L(L3Lpin);
-	static DigitalOut L3H(L3Hpin);
+    //Digital Motor Drive outputs
+    static DigitalOut L1L(L1Lpin); 
+    static DigitalOut L1H(L1Hpin);
+    static DigitalOut L2L(L2Lpin);
+    static DigitalOut L2H(L2Hpin);
+    static DigitalOut L3L(L3Lpin);
+    static DigitalOut L3H(L3Hpin);
 
-	// Variables for PWM controllers
-	static int PWM_counter = 0;
-	static Ticker pwm_ticker;
-	static Ticker pwm_loop;
-	static volatile bool update = false;
-	static volatile bool do_pwm = false;
+    // Variables for PWM controllers
+    static int PWM_counter = 0;
+    static Ticker pwm_ticker;
+    static Ticker pwm_loop;
+    static volatile bool update = false;
+    static volatile bool do_pwm = false;
 
 
-	// Serial output for debugging
-	static Serial debug(SERIAL_TX, SERIAL_RX, 9600);	
+    // Serial output for debugging
+    static Serial debug(SERIAL_TX, SERIAL_RX, 9600);    
 
-	// state sync value
-	static volatile int8_t base_state;  
-	static volatile int8_t curr_state;
-	static volatile int8_t prev_state;
+    // state sync value
+    static volatile int8_t base_state;  
+    static volatile int8_t curr_state;
+    static volatile int8_t prev_state;
 
-	// Op code variable to handle input
-	parser::update_t curr_op = OP_NIL;
+    // Op code variable to handle input
+    parser::update_t curr_op = parser::OP_NIL;
 
-	// Variables for tune playing
-	static float beat_period = 0.5;
-	
-	//Set a given drive state
-	static void motorOut(int8_t driveState){
-		
-		//Lookup the output byte from the drive state.
-		int8_t driveOut = driveTable[driveState & 0x07];
-		int8_t ndriveOut = ~driveOut; 
-		
-		// Normal Speed Setting
-		//Turn off first
-		if (ndriveOut & 0x01) L1L = 0;
-		if (ndriveOut & 0x02) L1H = 1;
-		if (ndriveOut & 0x04) L2L = 0;
-		if (ndriveOut & 0x08) L2H = 1;
-		if (ndriveOut & 0x10) L3L = 0;
-		if (ndriveOut & 0x20) L3H = 1;
-		
-		//Then turn on
-		if (driveOut & 0x01) L1L = 1;
-		if (driveOut & 0x02) L1H = 0;
-		if (driveOut & 0x04) L2L = 1;
-		if (driveOut & 0x08) L2H = 0;
-		if (driveOut & 0x10) L3L = 1;
-		if (driveOut & 0x20) L3H = 0;   
-	}
-		
-	//Convert photointerrupter inputs to a rotor state
-	static inline int8_t readRotorState(){
+    // Variables for tune playing
+    static float beat_period = 0.5;
+    
+    //Set a given drive state
+    static void motorOut(int8_t driveState){
+        
+        //Lookup the output byte from the drive state.
+        int8_t driveOut = driveTable[driveState & 0x07];
+        int8_t ndriveOut = ~driveOut; 
+        
+        // Normal Speed Setting
+        //Turn off first
+        if (ndriveOut & 0x01) L1L = 0;
+        if (ndriveOut & 0x02) L1H = 1;
+        if (ndriveOut & 0x04) L2L = 0;
+        if (ndriveOut & 0x08) L2H = 1;
+        if (ndriveOut & 0x10) L3L = 0;
+        if (ndriveOut & 0x20) L3H = 1;
+        
+        //Then turn on
+        if (driveOut & 0x01) L1L = 1;
+        if (driveOut & 0x02) L1H = 0;
+        if (driveOut & 0x04) L2L = 1;
+        if (driveOut & 0x08) L2H = 0;
+        if (driveOut & 0x10) L3L = 1;
+        if (driveOut & 0x20) L3H = 0;   
+    }
+        
+    //Convert photointerrupter inputs to a rotor state
+    static inline int8_t readRotorState(){
 
-		return stateMap[odometer::I1 + 2*odometer::I2 + 4*odometer::I3];
-	}
+        return stateMap[odometer::I1 + 2*odometer::I2 + 4*odometer::I3];
+    }
 
-	static int8_t motorHome(){
-		motorOut(0);
-		wait(1);
-		return readRotorState();	
-	}
+    static int8_t motorHome(){
+        motorOut(0);
+        wait(1);
+        return readRotorState();    
+    }
 
-	static void pwm_isr(){
-		PWM_counter = (PWM_counter == 100) ? 100 : ++PWM_counter;
-		pwm_ticker.attach(&pwm_isr, PWM_TICK);
-	}
+    static void pwm_isr(){
+        PWM_counter = (PWM_counter == 100) ? 100 : ++PWM_counter;
+        pwm_ticker.attach(&pwm_isr, PWM_TICK);
+    }
 
-	static void pwm_reset(){
-		PWM_counter = 0;
-		do_pwm = false;
-	}
+    static void pwm_reset(){
+        PWM_counter = 0;
+        do_pwm = false;
+    }
 
-	static void playTune(int half_period, float duration){
-		Timer beat;
-		beat.start();
+    static void playTune(int half_period, float duration){
+        Timer beat;
+        beat.start();
 
-		while (beat.read() < duration) {
-			motorOut(prev_state);
-			Thread::wait(half_period);
-			motorOut(curr_state);
-			Thread::wait(half_period);
-		}
-	}
+        while (beat.read() < duration) {
+            motorOut(prev_state);
+            Thread::wait(half_period);
+            motorOut(curr_state);
+            Thread::wait(half_period);
+        }
+    }
 
-	static void drivePWM(){
-		//Timer pwm_timer;
-		float duty_cycle = controller::duty_cycle;
-		int8_t delta = (duty_cycle > 0) ? 2 : -2;
+    static void drivePWM(){
+        //Timer pwm_timer;
+        float duty_cycle = controller::duty_cycle;
+        int8_t delta = (duty_cycle > 0) ? 2 : -2;
 
-		duty_cycle = std::abs(duty_cycle);
-		do_pwm = true;
+        duty_cycle = std::abs(duty_cycle);
+        do_pwm = true;
 
-		pwm_ticker.attach(&pwm_isr, PWM_TICK);
-		pwm_loop.attach(&pwm_reset, PWM_PERIOD);
-		// pwm_timer.start();
+        pwm_ticker.attach(&pwm_isr, PWM_TICK);
+        pwm_loop.attach(&pwm_reset, PWM_PERIOD);
+        // pwm_timer.start();
 
-		while(do_pwm){
-			//pwm_timer.reset();
-			curr_state = readRotorState();
+        while(do_pwm){
+            //pwm_timer.reset();
+            curr_state = readRotorState();
 
-			if((PWM_counter < duty_cycle) && (curr_state != prev_state)){
-				prev_state = curr_state;
-				motorOut((curr_state-base_state+delta+6)%6);
-			}
+            if((PWM_counter < duty_cycle) && (curr_state != prev_state)){
+                prev_state = curr_state;
+                motorOut((curr_state-base_state+delta+6)%6);
+            }
 
-			// float readout = pwm_timer.read();
-			// Thread::wait((readout < PWM_TICK) ? PWM_TICK - readout : 0);
-		}
+            // float readout = pwm_timer.read();
+            // Thread::wait((readout < PWM_TICK) ? PWM_TICK - readout : 0);
+        }
 
-		pwm_ticker.detach();
-	}
-	
-	void runMotor() {
-		Timer loop;
+        pwm_ticker.detach();
+    }
+    
+    void runMotor() {
+        Timer loop;
+        parser::update_t new_op = parser::OP_NIL;
+        loop.start();
+        while(1) {
+            
+            if (parser::ready[DRVR_INDEX]) {
+                // Polls for any updates to the opcode and handles changes appropriately
+                new_op = parser::op_code;
+                parser::ready[DRVR_INDEX] = false;
+            }
 
-		loop.start();
-		while(1) {
-			
-			if (parser::ready[DRVR_INDEX]) {
-				// Polls for any updates to the opcode and handles changes appropriately
-				parser::update_t new_op = parser::op_code;
-				parser::ready[DRVR_INDEX] = false;
-			}
+            if (new_op == parser::OP_BPM) {
+                beat_period = parser::tune_period;
 
-			if (new_op == parser::OP_BPM) {
-			 	beat_period = parser::tune_period;
+            } else if (new_op != parser::OP_NIL) {
+                curr_op = new_op;
 
-			} else if (new_op != parser::OP_NIL) {
-			 	curr_op = new_op;
+            }
 
-			}
+            // Executes stuff if new_op involves driving the motors
+            switch (curr_op) {
+                case parser::OP_TUNE:
+                    //play tune
+                    for (int i = 0; i < TUNE_BUFFER; i++) {
+                        int half_period = parser::tunes_list[i];
+                        if (half_period) {
+                            playTune(half_period, beat_period * 0.9);
+                            Thread::wait(int(beat_period * 100)); // beat_period * 0.1 * 1000ms
+                        } else {
+                            Thread::wait(int(beat_period * 1000));
+                        }            
+                    }
 
-			// Executes stuff if new_op involves driving the motors
-			switch (curr_op) {
-				case parser::OP_TUNE:
-					//play tune
-					for (int i = 0; i < TUNE_BUFFER; i++) {
-						int half_period = parser::tunes_list[i];
-						if (half_period) {
-							playTune(half_period, beat_period * 0.9);
-							Thread::wait(int(beat_period * 100)); // beat_period * 0.1 * 1000ms
-						} else {
-							Thread::wait(int(beat_period * 1000));
-						}			 
-					}
+                    break;
+                
+                case parser::OP_NIL:
+                    Thread::wait(PWM_PERIOD);
+                    break;
+                //run PWM for 1 cycle
+                default:
+                    drivePWM();
 
-					break;
+                    float readout = loop.read();
+                    loop.reset();
+                    Thread::wait((readout < PWM_PERIOD) ? PWM_PERIOD - readout : 0);
+                    break;
+            }
 
-				//run PWM for 1 cycle
-				default:
-					drivePWM();
+        }
+    }
 
-					float readout = loop.read();
-					loop.reset();
-					Thread::wait((readout < PWM_PERIOD) ? PWM_PERIOD - readout : 0);
-					break;
-			}
+    void init(){
 
-		}
-	}
-
-	void init(){
-
-		base_state = motorHome();
-		curr_state = base_state;
-		prev_state = (base_state - 1) % 6;
-	}
+        base_state = motorHome();
+        curr_state = base_state;
+        prev_state = (base_state - 1) % 6;
+    }
 }
